b25026ce3b03a4e82e68d44ec13fc58978b01697
Jeffrey Zhang <pengtuzhang@gmail.com>
2025-01-17 23:01:31 +1100

    excluded cdk from build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
   },
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["node_modules", "cdk"], // Add packages you want to exclude from optimization
+  },
+  build: {
+    rollupOptions: {
+      external: ["node_modules", "cdk"], // Add packages you want to exclude from the build
+    },
+  },
 })
